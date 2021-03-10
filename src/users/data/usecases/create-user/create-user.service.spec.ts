4726094cb6230b944a657a78611f9f5d26e22454
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-10 01:25:54 -0300

    test: ensure CreateUserService call Hash with correct plaintext

--- src/users/data/usecases/create-user/create-user.service.spec.ts
+++ src/users/data/usecases/create-user/create-user.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { Hash } from '~/shared/data';
 import { CreateUserModel } from '~/users/domain';
 
 import { CreateUserRepository } from '../../repositories';
@@ -7,9 +8,11 @@ import { CreateUserService } from './create-user.service';
 
 describe('CreateUserService', () => {
   const createUserRepositoryMock = () => ({ create: jest.fn() });
+  const hashMock = () => ({ make: jest.fn() });
 
   let sut: CreateUserService;
   let createUserRepository: CreateUserRepository;
+  let hash: Hash;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,12 +21,14 @@ describe('CreateUserService', () => {
         {
           provide: 'CreateUserRepository',
           useFactory: createUserRepositoryMock
-        }
+        },
+        { provide: 'Hash', useFactory: hashMock }
       ]
     }).compile();
 
     sut = module.get(CreateUserService);
     createUserRepository = module.get('CreateUserRepository');
+    hash = module.get('Hash');
   });
 
   it('should be defined', () => {
@@ -55,6 +60,10 @@ describe('CreateUserService', () => {
       password: 'any_password',
       email: 'any_mail@mail.com'
     };
+
+    jest
+      .spyOn(hash, 'make')
+      .mockReturnValueOnce(Promise.resolve('any_password'));
 
     jest.spyOn(createUserRepository, 'create').mockReturnValueOnce(
       Promise.resolve({
@@ -94,5 +103,27 @@ describe('CreateUserService', () => {
       });
 
     await expect(sut.execute(createUserModel)).rejects.toThrow();
+  });
+
+  it('should call Hash with correct plaintext', async () => {
+    const createUserModel: CreateUserModel = {
+      firstname: 'any_firstname',
+      lastname: 'any_lastname',
+      password: 'any_password',
+      email: 'any_mail@mail.com'
+    };
+
+    const hashedPasswordMock = 'hashed_any_password';
+    jest
+      .spyOn(hash, 'make')
+      .mockReturnValueOnce(Promise.resolve(hashedPasswordMock));
+    jest.spyOn(createUserRepository, 'create');
+
+    await sut.execute(createUserModel);
+
+    expect(hash.make).toBeCalledWith(createUserModel.password);
+    expect(createUserRepository.create).toBeCalledWith(
+      expect.objectContaining({ password: hashedPasswordMock })
+    );
   });
 });
