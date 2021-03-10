459048468179c58e969c6d183a6c00492ad9867f
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-10 01:04:39 -0300

    feat: ensure CreateUserService should return a new user

--- src/users/data/usecases/create-user/create-user.service.ts
+++ src/users/data/usecases/create-user/create-user.service.ts
@@ -4,8 +4,21 @@ import { CreateUser, CreateUserModel, UserModel } from '~/users/domain';
 
 @Injectable()
 export class CreateUserService implements CreateUser {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  execute(createUser: CreateUserModel): Promise<UserModel> {
-    throw new Error('Method not implemented.');
+  async execute({
+    firstname,
+    lastname,
+    email,
+    password
+  }: CreateUserModel): Promise<UserModel> {
+    return {
+      id: 'id',
+      uuid: 'uuid',
+      firstname,
+      lastname,
+      password,
+      email,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
   }
 }
