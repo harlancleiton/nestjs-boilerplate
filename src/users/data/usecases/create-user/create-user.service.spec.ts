6480d42b35067d855bd1e4c10f4cd83c3dbcfc85
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-10 01:05:16 -0300

    test: ensure CreateUserService should return a new user

--- src/users/data/usecases/create-user/create-user.service.spec.ts
+++ src/users/data/usecases/create-user/create-user.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { CreateUserModel } from '~/users/domain';
+
 import { CreateUserService } from './create-user.service';
 
 describe('CreateUserService', () => {
@@ -15,5 +17,23 @@ describe('CreateUserService', () => {
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+  });
+
+  it('should return a new user', async () => {
+    const createUserModel: CreateUserModel = {
+      firstname: 'any_firstname',
+      lastname: 'any_lastname',
+      password: 'any_password',
+      email: 'any_mail@mail.com'
+    };
+
+    const user = await sut.execute(createUserModel);
+
+    expect(user).toBeDefined();
+    expect(user).toMatchObject({
+      firstname: 'any_firstname',
+      lastname: 'any_lastname',
+      email: 'any_mail@mail.com'
+    });
   });
 });
