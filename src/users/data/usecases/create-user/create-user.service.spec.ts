658277a6d5740708c2bada2d243f3711fecf1e98
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-10 01:16:39 -0300

    test: ensure CreateUserService call CreateUserRepository with correct values

--- src/users/data/usecases/create-user/create-user.service.spec.ts
+++ src/users/data/usecases/create-user/create-user.service.spec.ts
@@ -2,17 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { CreateUserModel } from '~/users/domain';
 
+import { CreateUserRepository } from '../../repositories';
 import { CreateUserService } from './create-user.service';
 
 describe('CreateUserService', () => {
+  const createUserRepositoryMock = () => ({ create: jest.fn() });
+
   let sut: CreateUserService;
+  let createUserRepository: CreateUserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateUserService]
+      providers: [
+        CreateUserService,
+        {
+          provide: 'CreateUserRepository',
+          useFactory: createUserRepositoryMock
+        }
+      ]
     }).compile();
 
     sut = module.get(CreateUserService);
+    createUserRepository = module.get('CreateUserRepository');
   });
 
   it('should be defined', () => {
@@ -33,6 +44,37 @@ describe('CreateUserService', () => {
     expect(user).toMatchObject({
       firstname: 'any_firstname',
       lastname: 'any_lastname',
+      email: 'any_mail@mail.com'
+    });
+  });
+
+  it('should call CreateUserRepository with correct values', async () => {
+    const createUserModel: CreateUserModel = {
+      firstname: 'any_firstname',
+      lastname: 'any_lastname',
+      password: 'any_password',
+      email: 'any_mail@mail.com'
+    };
+
+    jest.spyOn(createUserRepository, 'create').mockReturnValueOnce(
+      Promise.resolve({
+        id: 'id',
+        uuid: 'uuid',
+        firstname: 'any_firstname',
+        lastname: 'any_lastname',
+        password: 'any_password',
+        email: 'any_mail@mail.com',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+    );
+
+    await sut.execute(createUserModel);
+
+    expect(createUserRepository.create).toBeCalledWith({
+      firstname: 'any_firstname',
+      lastname: 'any_lastname',
+      password: 'any_password',
       email: 'any_mail@mail.com'
     });
   });
