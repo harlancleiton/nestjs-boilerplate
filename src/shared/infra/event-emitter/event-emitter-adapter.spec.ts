448aa3b60f120d82a95c9d4dedb32ad9fc92cb17
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-11 11:39:40 -0300

    test: ensure EventEmitterAdapter should call EventEmitter.emit with correct values

--- src/shared/infra/event-emitter/event-emitter-adapter.spec.ts
+++ src/shared/infra/event-emitter/event-emitter-adapter.spec.ts
@@ -1,19 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { EventEmitter2 } from 'eventemitter2';
+
+import { factories } from '~/test/factories';
+
 import { EventEmitterAdapter } from './event-emitter-adapter';
 
 describe('EventEmitterAdapter', () => {
+  const eventEmitterMock = () => ({ emit: jest.fn() });
+
   let sut: EventEmitterAdapter;
+  let eventEmitter: EventEmitter2;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EventEmitterAdapter]
+      providers: [
+        EventEmitterAdapter,
+        { provide: EventEmitter2, useFactory: eventEmitterMock }
+      ]
     }).compile();
 
     sut = module.get(EventEmitterAdapter);
+    eventEmitter = module.get(EventEmitter2);
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+  });
+
+  it('should call EventEmitter.emit with correct values', () => {
+    jest.spyOn(eventEmitter, 'emit');
+
+    const event = factories.faker.lorem.word();
+    const eventValue = factories.userModel.build();
+
+    sut.emit(event, eventValue);
+
+    expect(eventEmitter.emit).toBeCalledWith(event, eventValue);
   });
 });
