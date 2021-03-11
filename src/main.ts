a8b7c24372a2c98b1d718f18b5e6e5a1d50e39e3
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-11 00:20:35 -0300

    feat: start server with the port provided by ConfigService

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const port = Number(configService.get('PORT'));
+  await app.listen(port);
 }
 
 bootstrap();
