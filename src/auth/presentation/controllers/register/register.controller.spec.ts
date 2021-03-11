989fd7365f74ac2a9e2d2de0f8efd90c82f9ae9b
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-10 21:01:30 -0300

    test: ensure RegisterController should call CreateUser with correct values

--- src/auth/presentation/controllers/register/register.controller.spec.ts
+++ src/auth/presentation/controllers/register/register.controller.spec.ts
@@ -1,19 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { plainToClass } from 'class-transformer';
+
+import { factories } from '~/test/factories';
+import { CreateUser } from '~/users/domain';
+
+import { CreateUserDto } from '../../dtos';
 import { RegisterController } from './register.controller';
 
 describe('RegisterController', () => {
+  const createUserMock = () => ({ execute: jest.fn() });
+
   let sut: RegisterController;
+  let createUser: CreateUser;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [RegisterController]
+      controllers: [RegisterController],
+      providers: [{ provide: 'CreateUser', useFactory: createUserMock }]
     }).compile();
 
     sut = module.get(RegisterController);
+    createUser = module.get('CreateUser');
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+  });
+
+  it('should call CreateUser with correct values', async () => {
+    const createUserModel = plainToClass(
+      CreateUserDto,
+      factories.createUserModel.build()
+    );
+
+    jest.spyOn(createUser, 'execute');
+
+    await sut.store(createUserModel);
+
+    expect(createUser.execute).toBeCalledWith(createUserModel);
   });
 });
