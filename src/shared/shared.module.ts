0066bd0ed537fba86c02f5af413716f1decd2ddb
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-10 23:02:16 -0300

    chore: add nestjs config module

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,26 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import * as Joi from 'joi';
 
 import { BCryptAdapter } from './infra';
 
 @Global()
-@Module({ providers: [BCryptAdapter] })
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+      validationOptions: { abortEarly: true },
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().required(),
+        SALT_ROUNDS: Joi.number().default(10)
+      })
+    })
+  ],
+  providers: [BCryptAdapter]
+})
 export class SharedModule {}
