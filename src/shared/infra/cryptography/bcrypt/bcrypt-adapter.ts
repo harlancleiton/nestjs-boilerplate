50fe15829a63839ee4d9424a80e8319dd1862408
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-10 21:25:21 -0300

    feat: ensure BCryptAdapter should call hash with correct values

--- src/shared/infra/cryptography/bcrypt/bcrypt-adapter.ts
+++ src/shared/infra/cryptography/bcrypt/bcrypt-adapter.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
+import * as bcrypt from 'bcryptjs';
+
 import { Hash } from '~/shared/data';
 
 @Injectable()
 export class BCryptAdapter implements Hash {
   async make(plaintext: string): Promise<string> {
+    const salt = await bcrypt.genSalt(12);
+    await bcrypt.hash(plaintext, salt);
+
     return plaintext;
   }
 
