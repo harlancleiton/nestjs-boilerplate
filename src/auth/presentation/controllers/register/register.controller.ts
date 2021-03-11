c6df7596b4dc0184a4df5a326a7c5f545f990a1c
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-11 00:16:23 -0300

    feat: add Post decorator and fix route

--- src/auth/presentation/controllers/register/register.controller.ts
+++ src/auth/presentation/controllers/register/register.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 
 import { plainToClass } from 'class-transformer';
 
@@ -6,13 +6,14 @@ import { CreateUser } from '~/users/domain';
 
 import { CreateUserDto, UserDto } from '../../dtos';
 
-@Controller('register')
+@Controller('auth/register')
 export class RegisterController {
   constructor(
     @Inject('CreateUser')
     private readonly createUser: CreateUser
   ) {}
 
+  @Post()
   async store(@Body() createUserDto: CreateUserDto): Promise<UserDto> {
     const user = await this.createUser.execute(createUserDto);
 
