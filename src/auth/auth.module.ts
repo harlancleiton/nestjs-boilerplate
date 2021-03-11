bcfc60e700ce1a89bb5d0ce13fc2c81900b798a4
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-11 00:14:49 -0300

    chore: add  UsersModule to imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { UsersModule } from '~/users';
+
 import { RegisterController } from './presentation';
 
 @Module({
+  imports: [UsersModule],
   controllers: [RegisterController]
 })
 export class AuthModule {}
