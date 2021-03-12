38ea4e90b675bec95ee53ad2555457510a45a131
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-12 03:27:31 -0300

    feat: ensure ValidateLoginService call FindUserByEmailRepository with correct values

--- src/auth/data/usecases/validate-login/validate-login.service.ts
+++ src/auth/data/usecases/validate-login/validate-login.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
 import { ValidateLogin } from '~/auth/domain';
-import { UserModel } from '~/users/domain';
+import { FindUserByEmailRepository } from '~/users/data';
+import { UserModel, UsersRepositoryConstants } from '~/users/domain';
 
 @Injectable()
 export class ValidateLoginService implements ValidateLogin {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  execute(email: string, password: string): Promise<UserModel | undefined> {
-    throw new Error('Method not implemented.');
+  constructor(
+    @Inject(UsersRepositoryConstants.FIND_USER_BY_EMAIL_REPOSITORY)
+    private readonly findUserByEmailRepository: FindUserByEmailRepository
+  ) {}
+
+  async execute(
+    email: string,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    password: string
+  ): Promise<UserModel | undefined> {
+    await this.findUserByEmailRepository.findByEmail(email);
+
+    return undefined;
   }
 }
