99f7aed904bfed3a9c2fa11fe6ba8d87166a3508
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-12 00:22:27 -0300

    test: ensure LocalStrategy call ValidateLogin with correct values

--- src/auth/presentation/strategies/local/local.strategy.spec.ts
+++ src/auth/presentation/strategies/local/local.strategy.spec.ts
@@ -1,16 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { AuthUseCasesConstants, ValidateLogin } from '~/auth/domain';
+import { factories } from '~/test/factories';
+
 import { LocalStrategy } from './local.strategy';
 
 describe('LocalStrategy', () => {
+  const validateLoginMock = () => ({ execute: jest.fn() });
+
   let sut: LocalStrategy;
+  let validateLogin: ValidateLogin;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LocalStrategy]
+      providers: [
+        LocalStrategy,
+        {
+          provide: AuthUseCasesConstants.VALIDATE_LOGIN,
+          useFactory: validateLoginMock
+        }
+      ]
     }).compile();
 
     sut = module.get(LocalStrategy);
+    validateLogin = module.get(AuthUseCasesConstants.VALIDATE_LOGIN);
   });
 
   it('should be defined', () => {
@@ -25,5 +38,16 @@ describe('LocalStrategy', () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     expect(sut._passwordField).toBe('password');
+  });
+
+  it('should call ValidateLogin with correct values', async () => {
+    const email = factories.faker.internet.email();
+    const password = factories.faker.internet.password();
+
+    jest.spyOn(validateLogin, 'execute');
+
+    await sut.validate(email, password);
+
+    expect(validateLogin.execute).toBeCalledWith(email, password);
   });
 });
