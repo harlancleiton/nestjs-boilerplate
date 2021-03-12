230ccfa4f1f759a023a5287f51f169c56b5d4342
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-12 13:10:41 -0300

    test: ensure LoginController should call GenerateJwtToken with correct values

--- src/auth/presentation/controllers/login/login.controller.spec.ts
+++ src/auth/presentation/controllers/login/login.controller.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { AuthUseCasesConstants } from '~/auth/domain';
+import { plainToClass } from 'class-transformer';
 
+import { AuthUseCasesConstants, GenerateJwtToken } from '~/auth/domain';
+import { factories } from '~/test/factories';
+
+import { UserDto } from '../../dtos';
 import { LoginController } from './login.controller';
 
 describe('LoginController', () => {
   const generateJwtTokenMock = () => ({ execute: jest.fn() });
 
   let sut: LoginController;
+  let generateJwtToken: GenerateJwtToken;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,9 +26,20 @@ describe('LoginController', () => {
     }).compile();
 
     sut = module.get(LoginController);
+    generateJwtToken = module.get(AuthUseCasesConstants.GENERATE_JWT_TOKEN);
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+  });
+
+  it('should call GenerateJwtToken with correct values', async () => {
+    const user = plainToClass(UserDto, factories.userModel.build());
+
+    jest.spyOn(generateJwtToken, 'execute');
+
+    await sut.store({ user });
+
+    expect(generateJwtToken.execute).toBeCalledWith(user);
   });
 });
