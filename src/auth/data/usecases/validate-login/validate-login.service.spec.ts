cf31871abcae81cde8646a7da0d4d1df44a9307f
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-12 10:42:11 -0300

    test: ensure ValidateLoginService calls Hash.compare with correct values

--- src/auth/data/usecases/validate-login/validate-login.service.spec.ts
+++ src/auth/data/usecases/validate-login/validate-login.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { Hash } from '~/shared/data';
+import { AdaptersConstants } from '~/shared/domain';
 import { factories } from '~/test/factories';
 import { FindUserByEmailRepository } from '~/users/data';
 import { UsersRepositoryConstants } from '~/users/domain';
@@ -8,9 +10,11 @@ import { ValidateLoginService } from './validate-login.service';
 
 describe('ValidateLoginService', () => {
   const findUserByEmailRepositoryMock = () => ({ findByEmail: jest.fn() });
+  const hashMock = () => ({ compare: jest.fn() });
 
   let sut: ValidateLoginService;
   let findUserByEmailRepository: FindUserByEmailRepository;
+  let hash: Hash;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,7 +23,8 @@ describe('ValidateLoginService', () => {
         {
           provide: UsersRepositoryConstants.FIND_USER_BY_EMAIL_REPOSITORY,
           useFactory: findUserByEmailRepositoryMock
-        }
+        },
+        { provide: AdaptersConstants.HASH, useFactory: hashMock }
       ]
     }).compile();
 
@@ -27,6 +32,7 @@ describe('ValidateLoginService', () => {
     findUserByEmailRepository = module.get(
       UsersRepositoryConstants.FIND_USER_BY_EMAIL_REPOSITORY
     );
+    hash = module.get(AdaptersConstants.HASH);
   });
 
   it('should be defined', () => {
@@ -68,5 +74,20 @@ describe('ValidateLoginService', () => {
       });
 
     await expect(sut.execute(email, password)).rejects.toThrow();
+  });
+
+  it('should call Hash.compare with correct values', async () => {
+    const email = factories.faker.internet.email();
+    const password = factories.faker.internet.password();
+    const userModel = factories.userModel.build();
+
+    jest.spyOn(hash, 'compare');
+    jest
+      .spyOn(findUserByEmailRepository, 'findByEmail')
+      .mockReturnValueOnce(Promise.resolve(userModel));
+
+    await sut.execute(email, password);
+
+    expect(hash.compare).toBeCalledWith(password, userModel.password);
   });
 });
