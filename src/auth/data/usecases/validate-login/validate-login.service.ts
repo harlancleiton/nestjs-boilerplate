7932d34e21e81d7b4c268c85ff9643174f0150ba
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-12 10:41:21 -0300

    feat: ensure ValidateLoginService calls Hash.compare with correct values

--- src/auth/data/usecases/validate-login/validate-login.service.ts
+++ src/auth/data/usecases/validate-login/validate-login.service.ts
@@ -1,6 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 
 import { ValidateLogin } from '~/auth/domain';
+import { Hash } from '~/shared/data';
+import { AdaptersConstants } from '~/shared/domain';
 import { FindUserByEmailRepository } from '~/users/data';
 import { UserModel, UsersRepositoryConstants } from '~/users/domain';
 
@@ -8,7 +10,9 @@ import { UserModel, UsersRepositoryConstants } from '~/users/domain';
 export class ValidateLoginService implements ValidateLogin {
   constructor(
     @Inject(UsersRepositoryConstants.FIND_USER_BY_EMAIL_REPOSITORY)
-    private readonly findUserByEmailRepository: FindUserByEmailRepository
+    private readonly findUserByEmailRepository: FindUserByEmailRepository,
+    @Inject(AdaptersConstants.HASH)
+    private readonly hash: Hash
   ) {}
 
   async execute(
@@ -19,6 +23,8 @@ export class ValidateLoginService implements ValidateLogin {
     const user = await this.findUserByEmailRepository.findByEmail(email);
 
     if (!user) return undefined;
+
+    await this.hash.compare(password, user.password);
 
     return undefined;
   }
