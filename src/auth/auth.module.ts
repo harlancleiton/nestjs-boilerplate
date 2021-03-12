99c0616da3f6c455688f23f8a210de5a15919b6f
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-11 23:44:12 -0300

    feat: ensure JwtAuthGuard should be defined

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '~/users';
 
 import { RegisterController } from './presentation';
+import { JwtAuthGuard } from './presentation/guards';
 
 @Module({
   imports: [UsersModule],
-  controllers: [RegisterController]
+  controllers: [RegisterController],
+  providers: [JwtAuthGuard]
 })
 export class AuthModule {}
