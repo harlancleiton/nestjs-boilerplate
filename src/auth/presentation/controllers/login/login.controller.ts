7edc9fe1745afbc123fc67859dae26392231e438
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-12 16:22:52 -0300

    feat: add http decorators

--- src/auth/presentation/controllers/login/login.controller.ts
+++ src/auth/presentation/controllers/login/login.controller.ts
@@ -1,18 +1,21 @@
-import { Controller, Inject, Request } from '@nestjs/common';
+import { Controller, Inject, Post, Request, UseGuards } from '@nestjs/common';
 
 import { plainToClass } from 'class-transformer';
 
 import { AuthUseCasesConstants, GenerateJwtToken } from '~/auth/domain';
 
 import { LoginResponseDto } from '../../dtos';
+import { LocalAuthGuard } from '../../guards';
 
-@Controller('login')
+@Controller('/auth/login')
 export class LoginController {
   constructor(
     @Inject(AuthUseCasesConstants.GENERATE_JWT_TOKEN)
     private readonly generateJwtToken: GenerateJwtToken
   ) {}
 
+  @Post()
+  @UseGuards(LocalAuthGuard)
   async store(@Request() request): Promise<LoginResponseDto> {
     const { user } = request;
     const login = await this.generateJwtToken.execute(user);
