24b5198797635656564fc6bb0cb6c75ee4f25f31
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-12 00:01:45 -0300

    feat: ensure LocalStrategy should be defined

---
+++ src/auth/presentation/strategies/local/local.strategy.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+
+import { Strategy } from 'passport-local';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super();
+  }
+}
