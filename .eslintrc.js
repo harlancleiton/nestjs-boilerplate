a0dc98a8fc75dd10a422053086688047dbcd9e0e
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-16 10:17:24 -0300

    chore: add no-console rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     'prettier/prettier': 'error',
+    'no-console': 'warn',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
