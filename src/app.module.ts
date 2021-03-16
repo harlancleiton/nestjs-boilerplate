b94e813caa1805707e9aed3981bd8ac9a4b64c33
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-15 22:22:25 -0300

    chore: add nestjs bull module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
@@ -11,6 +13,17 @@ import { EmailModule } from './email/email.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({ keepConnectionAlive: true }),
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('REDIS_HOST'),
+          password: configService.get('REDIS_PASSWORD'),
+          port: Number(configService.get('REDIS_PORT')),
+          db: Number(configService.get('REDIS_DB'))
+        }
+      })
+    }),
     EventEmitterModule.forRoot(),
     AuthModule,
     SharedModule,
