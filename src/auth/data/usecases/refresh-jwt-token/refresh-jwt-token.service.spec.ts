59b8285c7520cf07b272a7c93e3867071578062d
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-17 12:25:23 -0300

    test: ensure RefreshJwtTokenService call Encrypter with correct value

--- src/auth/data/usecases/refresh-jwt-token/refresh-jwt-token.service.spec.ts
+++ src/auth/data/usecases/refresh-jwt-token/refresh-jwt-token.service.spec.ts
@@ -1,19 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { Encrypter } from '~/shared/data';
+import { AdaptersConstants } from '~/shared/domain';
+import { factories } from '~/test/factories';
+
 import { RefreshJwtTokenService } from './refresh-jwt-token.service';
 
 describe('RefreshJwtTokenService', () => {
+  const encrypterMock = () => ({ decrypt: jest.fn() });
+
   let sut: RefreshJwtTokenService;
+  let encrypter: Encrypter;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RefreshJwtTokenService]
+      providers: [
+        RefreshJwtTokenService,
+        { provide: AdaptersConstants.ENCRYPTER, useFactory: encrypterMock }
+      ]
     }).compile();
 
     sut = module.get(RefreshJwtTokenService);
+    encrypter = module.get(AdaptersConstants.ENCRYPTER);
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+  });
+
+  it('should be call Encrypter with correct value', async () => {
+    const encryptedToken = factories.faker.random.alphaNumeric(32);
+
+    await sut.execute(encryptedToken);
+
+    expect(encrypter.decrypt).toBeCalledWith(encryptedToken);
   });
 });
