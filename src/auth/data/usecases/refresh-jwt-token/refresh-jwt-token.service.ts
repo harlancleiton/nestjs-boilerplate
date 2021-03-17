bb998384e100396a10501fa146eb44abcdef2d6b
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-17 12:24:22 -0300

    feat: ensure RefreshJwtTokenService call Encrypter with correct value

--- src/auth/data/usecases/refresh-jwt-token/refresh-jwt-token.service.ts
+++ src/auth/data/usecases/refresh-jwt-token/refresh-jwt-token.service.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
 import { LoginModel, RefreshJwtToken } from '~/auth/domain';
+import { Encrypter } from '~/shared/data';
+import { AdaptersConstants } from '~/shared/domain';
 
 @Injectable()
 export class RefreshJwtTokenService implements RefreshJwtToken {
+  constructor(
+    @Inject(AdaptersConstants.ENCRYPTER)
+    private readonly encrypter: Encrypter
+  ) {}
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async execute(encryptedToken: string): Promise<LoginModel> {
-    throw new Error('Method not implemented.');
+    this.encrypter.decrypt(encryptedToken);
+
+    return undefined;
   }
 }
