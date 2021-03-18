15c52bac6d7e674676ec8aea7d7745e973c6f96d
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-18 10:50:25 -0300

    test: ensure RefreshJwtTokenService should be call FindRefreshTokenRepository with correct value

--- src/auth/data/usecases/refresh-jwt-token/refresh-jwt-token.service.spec.ts
+++ src/auth/data/usecases/refresh-jwt-token/refresh-jwt-token.service.spec.ts
@@ -1,27 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { AuthRepositoriesConstants } from '~/auth/domain';
 import { Encrypter } from '~/shared/data';
 import { AdaptersConstants } from '~/shared/domain';
 import { factories } from '~/test/factories';
 
+import { FindRefreshTokenRepository } from '../../repositories';
 import { RefreshJwtTokenService } from './refresh-jwt-token.service';
 
 describe('RefreshJwtTokenService', () => {
   const encrypterMock = () => ({ decrypt: jest.fn() });
+  const findRefreshTokenRepositoryMock = () => ({
+    findRefreshToken: jest.fn()
+  });
 
   let sut: RefreshJwtTokenService;
   let encrypter: Encrypter;
+  let findRefreshTokenRepository: FindRefreshTokenRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         RefreshJwtTokenService,
-        { provide: AdaptersConstants.ENCRYPTER, useFactory: encrypterMock }
+        { provide: AdaptersConstants.ENCRYPTER, useFactory: encrypterMock },
+        {
+          provide: AuthRepositoriesConstants.FIND_REFRESH_TOKEN_REPOSITORY,
+          useFactory: findRefreshTokenRepositoryMock
+        }
       ]
     }).compile();
 
     sut = module.get(RefreshJwtTokenService);
     encrypter = module.get(AdaptersConstants.ENCRYPTER);
+    findRefreshTokenRepository = module.get(
+      AuthRepositoriesConstants.FIND_REFRESH_TOKEN_REPOSITORY
+    );
   });
 
   it('should be defined', () => {
@@ -44,5 +57,18 @@ describe('RefreshJwtTokenService', () => {
     });
 
     await expect(sut.execute(encryptedToken)).rejects.toThrow();
+  });
+
+  it('should be call FindRefreshTokenRepository with correct value', async () => {
+    const encryptedToken = factories.faker.random.alphaNumeric(32);
+    const uuid = factories.faker.random.uuid();
+
+    jest.spyOn(encrypter, 'decrypt').mockReturnValueOnce(uuid);
+
+    jest.spyOn(findRefreshTokenRepository, 'findRefreshToken');
+
+    await sut.execute(encryptedToken);
+
+    expect(findRefreshTokenRepository.findRefreshToken).toBeCalledWith(uuid);
   });
 });
