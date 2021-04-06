8db39840b88d932172b736743898ba5bc820f3b5
Harlan Cleiton <harlancleiton@gmail.com>
2021-04-06 09:16:01 -0300

    feat: ensure RefreshTokenController should be call RefreshJwtToken with correct value

--- src/auth/presentation/controllers/refresh-token/refresh-token.controller.ts
+++ src/auth/presentation/controllers/refresh-token/refresh-token.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+
+import { AuthUseCasesConstants, RefreshJwtToken } from '~/auth/domain';
+
+import { LoginResponseDto, RequestRefreshTokenDto } from '../../dtos';
 
 @Controller('refresh-token')
-export class RefreshTokenController {}
+export class RefreshTokenController {
+  constructor(
+    @Inject(AuthUseCasesConstants.REFRESH_JWT_TOKEN)
+    private readonly refreshJwtToken: RefreshJwtToken
+  ) {}
+
+  async store(
+    requestRefreshToken: RequestRefreshTokenDto
+  ): Promise<LoginResponseDto> {
+    const { refreshToken } = requestRefreshToken;
+    await this.refreshJwtToken.execute(refreshToken);
+
+    return null;
+  }
+}
