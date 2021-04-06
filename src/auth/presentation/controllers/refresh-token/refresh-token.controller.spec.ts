7511cd512b76ac89ee7e90bafd696041138b11a0
Harlan Cleiton <harlancleiton@gmail.com>
2021-04-06 09:17:16 -0300

    test: ensure RefreshTokenController should be call RefreshJwtToken with correct value

--- src/auth/presentation/controllers/refresh-token/refresh-token.controller.spec.ts
+++ src/auth/presentation/controllers/refresh-token/refresh-token.controller.spec.ts
@@ -1,19 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { AuthUseCasesConstants, RefreshJwtToken } from '~/auth/domain';
+import { factories } from '~/test/factories';
+
 import { RefreshTokenController } from './refresh-token.controller';
 
 describe('RefreshTokenController', () => {
+  const refreshJwtTokenMock = () => ({ execute: jest.fn() });
+
   let sut: RefreshTokenController;
+  let refreshJwtToken: RefreshJwtToken;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [RefreshTokenController]
+      controllers: [RefreshTokenController],
+      providers: [
+        {
+          provide: AuthUseCasesConstants.REFRESH_JWT_TOKEN,
+          useFactory: refreshJwtTokenMock
+        }
+      ]
     }).compile();
 
     sut = module.get(RefreshTokenController);
+    refreshJwtToken = module.get(AuthUseCasesConstants.REFRESH_JWT_TOKEN);
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+  });
+
+  it('should be call RefreshJwtToken with correct values', async () => {
+    const refreshToken = factories.faker.random.alphaNumeric(32);
+
+    jest.spyOn(refreshJwtToken, 'execute');
+
+    await sut.store({ refreshToken });
+
+    expect(refreshJwtToken.execute).toBeCalledWith(refreshToken);
   });
 });
