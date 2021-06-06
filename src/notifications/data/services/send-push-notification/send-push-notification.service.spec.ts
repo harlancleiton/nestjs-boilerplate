3a8e91405bfc7ee413c78246dcae06dc8ea61012
Harlan Cleiton <harlancleiton@gmail.com>
2021-06-05 23:10:46 -0300

    test: ensure SendPushNotificationService call FindTokenNotificationByUser with correct values

--- src/notifications/data/services/send-push-notification/send-push-notification.service.spec.ts
+++ src/notifications/data/services/send-push-notification/send-push-notification.service.spec.ts
@@ -1,19 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { NotificationRepositoriesConstants } from '~/notifications/domain';
+import { factories } from '~/test/factories';
+
+import { FindTokenNotificationByUserRepository } from '../../repositories';
 import { SendPushNotificationService } from './send-push-notification.service';
 
 describe('SendPushNotificationService', () => {
+  const mockFindTokenNotificationByUserRepository = () => ({
+    findTokenNotificationByUser: jest.fn()
+  });
+
   let sut: SendPushNotificationService;
+  let findTokenNotificationByUserRepository: FindTokenNotificationByUserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SendPushNotificationService]
+      providers: [
+        SendPushNotificationService,
+        {
+          provide:
+            NotificationRepositoriesConstants.FIND_TOKEN_NOTIFICATION_BY_USER_REPOSITORY,
+          useFactory: mockFindTokenNotificationByUserRepository
+        }
+      ]
     }).compile();
 
     sut = module.get(SendPushNotificationService);
+    findTokenNotificationByUserRepository = module.get(
+      NotificationRepositoriesConstants.FIND_TOKEN_NOTIFICATION_BY_USER_REPOSITORY
+    );
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+  });
+
+  it('should call FindTokenNotificationByUserRepository with correct values', async () => {
+    const notification = factories.notificationModel.build();
+
+    jest.spyOn(
+      findTokenNotificationByUserRepository,
+      'findTokenNotificationByUser'
+    );
+
+    await sut.execute(notification);
+
+    const { user } = notification;
+
+    expect(
+      findTokenNotificationByUserRepository.findTokenNotificationByUser
+    ).toBeCalledWith(user);
   });
 });
